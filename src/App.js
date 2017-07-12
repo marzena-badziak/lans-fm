c9d969cc3be1c931d3a3b8f8c0b3301789a122f8
Paweł Jastrzębski <pawjastrzebski@gmail.com>
2017-07-12 13:52:34 +0200

    added fetching data from api through simple  axios get

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
-import SearchBar from "./SearchBar";
+import SearchBar from "./user-interface/SearchBar";
 import ArtistsGrid from "./ArtistsGrid";
 
 class App extends Component {
