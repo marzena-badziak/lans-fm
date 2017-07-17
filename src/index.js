ee0bdad29b48af79781f92f62d6a20a57c7fc9c3
Kacper Ciecinski <ciecinski.kacper@gmail.com>
2017-07-17 10:53:41 +0200

    color pallate changed and code cleaned a bit

--- src/index.js
+++ src/index.js
@@ -2,20 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 //import { configureApi } from "./lib/api-client.js";
-import store from "./user-interface/store";
+import store from "./store";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 // configureApi(store);
 
 const AppWithStore = (
-<MuiThemeProvider>
-  <Provider store={store}>
-    <App store={store} />
-  </Provider>
-</MuiThemeProvider>
-
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <App store={store} />
+    </Provider>
+  </MuiThemeProvider>
 );
 
 ReactDOM.render(AppWithStore, document.getElementById("root"));
