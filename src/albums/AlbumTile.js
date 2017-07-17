ece7472c96937f5f4919f91fa2c8f802ba96cf5a
Kacper Ciecinski <ciecinski.kacper@gmail.com>
2017-07-17 14:05:14 +0200

    albums page added

---
+++ src/albums/AlbumTile.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardTitle,
+  CardText
+} from "material-ui/Card";
+import styled from "styled-components";
+import FlatButton from "material-ui/FlatButton";
+class AlbumTile extends Component {
+  render() {
+    return (
+      <StyledAlbumCard>
+        <CardMedia
+          overlay={
+            <CardTitle title={this.props.title} subtitle={this.props.artist} />
+          }
+        >
+          <img src={this.props.image} alt={`${this.props.title} cover`} />
+        </CardMedia>
+        <CardActions>
+          <FlatButton label="Scrobble" />
+          <FlatButton label="Show Album" />
+        </CardActions>
+      </StyledAlbumCard>
+    );
+  }
+}
+const StyledAlbumCard = styled(Card)`
+width: 300px;
+`;
+export default AlbumTile;
