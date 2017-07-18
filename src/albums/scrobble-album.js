bcee14b6f7d16b9a2efeead2f2baae73bfa19422
pawjastrzebski <pawjastrzebski@gmail.com>
2017-07-18 12:39:59 +0200

    -cleared javascript console -scrobbling function (not yet tested)

--- src/albums/scrobble-album.js
+++ src/albums/scrobble-album.js
@@ -1,5 +1,5 @@
 import _ from "lodash"
-
+//
 export const scrobbleAlbum = (album) => {
   let scrobbleRequest = "";
   let timestampOfScrobble = _.now();
@@ -8,4 +8,4 @@ export const scrobbleAlbum = (album) => {
     scrobbleRequest +=
       `&track[${track.rank - 1}]=${track.name}&artist[${track.rank - 1}]=${track.artist.name}&timestamp[${track.rank - 1}]=${timestampOfScrobble}&mbid[${track.rank - 1}]=${track.mbid}`;
     });
-}
+}1
