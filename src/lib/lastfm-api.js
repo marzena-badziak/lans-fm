af52440fb912cfde20fbae152070fb59bea78e1f
marzena-badziak <marzena.badziak@gmail.com>
2017-07-18 14:41:07 +0200

    deleted lines from lastfm-api that ar not ready

--- src/lib/lastfm-api.js
+++ src/lib/lastfm-api.js
@@ -13,18 +13,18 @@ const lastfmApi = (method, options) => {
   return request;
 };
 
-const lastfmScrobble = scrobbleRequest => {
-  let request = `http://ws.audioscrobbler.com/2.0/?method=${method}`;
-  Object.entries(options).forEach(([key, value]) => {
-    request = `${request}&${key}=${value}`;
-  });
-  request = `${request}&api_key=${lastfmKey.api_key}&format=json`;
-  return request;
-};
+// const lastfmScrobble = scrobbleRequest => {
+//   let request = `http://ws.audioscrobbler.com/2.0/?method=${method}`;
+//   Object.entries(options).forEach(([key, value]) => {
+//     request = `${request}&${key}=${value}`;
+//   });
+//   request = `${request}&api_key=${lastfmKey.api_key}&format=json`;
+//   return request;
+// };
 
-let scrobbleRequest = "";
-scrobbleRequest += `&track[${i}]=${track.name}&artist[${i}]=${track.artist
-  .name}&timestamp[${i}]=&mbid[${i}]=${track.mbid}`;
+// let scrobbleRequest = "";
+// scrobbleRequest += `&track[${i}]=${track.name}&artist[${i}]=${track.artist
+//   .name}&timestamp[${i}]=&mbid[${i}]=${track.mbid}`;
 /*var apiClient = axios.create({
   baseURL: "http://ws.audioscrobbler.com/2.0/",
   headers: {
